3c21ef18fe66c06ee1ec79cc48e33b9523bd9f1f
Jacek20s <jacek20s@o2.pl>
2022-02-11 16:21:09 +0100

    10_InstrukcjeSkoku

---
+++ 10_InstrukcjeSkoku/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_InstrukcjeSkoku
+{
+    internal class Program
+    {
+        static int[] liczby = { 1, 3, 5, 6, 7, 44, 343, };
+        static void Main(string[] args)
+        {
+            
+            Method1();
+
+            //foreach (var item in liczby)
+            //{
+            //    Console.WriteLine(item);
+            //    if (item ==3)
+            //    {
+            //        Console.WriteLine("3");
+            //        continue;
+            //    }
+            //    if (item == 6)
+            //    {
+            //        Console.WriteLine("liczba to 6");
+            //        break;
+            //    }
+            //}
+            Console.ReadKey();
+
+        }
+
+        private static void Method1()
+        {
+            foreach (var liczba in liczby)
+            {
+                if (liczba==5)
+                {
+                    Console.WriteLine("liczba wynosi 8");
+                    return;
+                }
+            }
+            Console.WriteLine("test");
+        }
+    }
+}
