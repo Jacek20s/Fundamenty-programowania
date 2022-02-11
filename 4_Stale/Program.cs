d6f150b8e8dc596121ba247bed285dc5d33d6a43
Jacek20s <jacek20s@o2.pl>
2022-02-11 15:55:23 +0100

    4_Stale

---
+++ 4_Stale/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Stale
+{
+    internal class Program
+    {
+        const float PI = 3.1415f;
+        public static float R = 5f;
+        const string nazwa = "ABC";
+        static void Main(string[] args)
+        {
+            ObliczPoleKola();
+            Console.ReadKey();
+        }
+
+        public static void ObliczPoleKola()
+        {
+            float pole = PI * R * R;
+            Console.WriteLine($"Pole koła = {pole}");
+        }
+        public void Method1(string nazwaFirmy)
+        {
+
+        }
+        public void Method2()
+        {
+            Method1(nazwa);
+        }
+    }
+}
