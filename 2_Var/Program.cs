a371571d7bc71b878144f376ebbfd7537fc12e79
Jacek20s <jacek20s@o2.pl>
2022-02-11 15:52:42 +0100

    2_Var

---
+++ 2_Var/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte zmiennaMala;
+            short liczbaCalkowitaKrotka;
+            int liczbaCalkowita2;
+            long liczbaCalkowitaDluga;
+            float liczbaZmiennoprzecinkowaPojedynczejPrecyzji;
+            double liczbaZmiennoprzecinkowaPodwojnejPrecyzji;
+            bool zmiennaLogiczna;
+            char zmiennaZnak;
+            string zmiennaTekstowa = "tekst";
+            var zmiennaVar = "tekst";
+            Console.WriteLine(zmiennaVar);
+            Console.ReadLine();
+        }
+        public int liczba = 50;
+        public void Method1()
+        {
+            var zmienna1 = liczba;
+        }
+        public void Method2()
+        {
+            var zmienna1 = liczba;
+
+        }
+        public void Method3()
+        {
+            var zmienna1 = liczba;
+
+        }
+    }
+}
