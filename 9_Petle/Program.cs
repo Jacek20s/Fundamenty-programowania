2f0164c34fc340678e6271d7303c5b68eb721734
Jacek20s <jacek20s@o2.pl>
2022-02-11 16:01:21 +0100

    9_Petle

---
+++ 9_Petle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Petle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int length = 10;
+                int[] tabela = new int[] { 1, 2, 3, 4, 5 };
+                //for (int i = 0; i < length; i++)
+                //{
+                //    Console.WriteLine(i);
+                //}
+                //while (length>5)
+                //{
+                //    Console.WriteLine(length);
+                //    length--;
+                //}
+                //do
+                //{
+                //    Console.WriteLine(length);
+                //} while (length<5);
+                foreach (var item in tabela)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
