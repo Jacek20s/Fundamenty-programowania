ebd42af58bd3d6388e570d56b20de04a3aabb976
Jacek20s <jacek20s@o2.pl>
2022-02-11 17:09:29 +0100

    13_Enum

---
+++ 13_Enum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Imiona imiona = Imiona.Marcin;
+            Console.WriteLine(Imiona.Jacek);
+            switch (imiona)
+            {
+                case Imiona.Marcin:
+                    break;
+                case Imiona.Tomek:
+                    break;
+                case Imiona.Jacek:
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+        public enum Imiona
+        {
+            Marcin,
+            Tomek,
+            Jacek
+
+        }
+    }
+}
